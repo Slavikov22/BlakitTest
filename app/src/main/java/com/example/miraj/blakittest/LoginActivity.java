7e1ca59c960080bef8c230da33f596e479ad3359
Slava Yakovlev <Slavikov22@mail.ru>
2017-12-15 23:44:23 +0300

    Add VK login

--- app/src/main/java/com/example/miraj/blakittest/LoginActivity.java
+++ app/src/main/java/com/example/miraj/blakittest/LoginActivity.java
@@ -1,13 +1,46 @@
 package com.example.miraj.blakittest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
 
 public class LoginActivity extends AppCompatActivity {
+    String[] scope = new String[] {VKScope.FRIENDS, VKScope.GROUPS, VKScope.MESSAGES,
+            VKScope.PHOTOS, VKScope.WALL};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        VKSdk.login(this, scope);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                Toast.makeText(LoginActivity.this, "Success", Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onError(VKError error) {
+                Toast.makeText(LoginActivity.this, "Error", Toast.LENGTH_LONG).show();
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public void refresh(View view) {
+        VKSdk.login(this, scope);
     }
 }
